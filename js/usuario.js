9172c36a79093bb141a38761a80ba2fba5e4fb7d
Laura Natalia Jaimes <lnataliaj61@gmail.com>
2021-01-24 17:53:41 -0500

    FIN
    Todo bien lindo

--- js/usuario.js
+++ js/usuario.js
@@ -3,59 +3,90 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 
-  $(document).ready(function () {
-    $('#DateOfBirth').change(function () {
-      var now = new Date();   //Current Date
-      var past = new Date($('#DateOfBirth').val());  //Date of Birth
-      if (past >= now) {
-        alert('ERROR: La fecha que ingreso es MAYOR a la fecha actual');
-      return false;
-      }
-      var nowYear = now.getFullYear();  //Get current year
-      var pastYear = past.getFullYear();//Get Date of Birth year
-      var age = nowYear - pastYear;  //calculate the difference
-  $('#Age').val(age);
-  if (age < 18){
-        alert('Debe ser Mayor de Edad para completar este formulario');
-      return false;
-      }
-})
-})
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
 
 
- //Select Json
- $.ajax({
-  url: 'https://raw.githubusercontent.com/marcovega/colombia-json/master/colombia.min.json',
-  type: 'GET',
-  dataType: 'json',
-
-  success: function (json) {
-    var departmento = `<option value="" disabled selected>Departamento</option>`;
-    for (let item of json) {
-      departmento += `<option value="${json.indexOf(item)}">${item.departamento}</option>`;
+//Muestra la edad de la persona segun fecha de nacimiento
+$(document).ready(function () {
+  $('#DateOfBirth').change(function () {
+    var now = new Date();   //Current Date
+    var past = new Date($('#DateOfBirth').val());  //Date of Birth
+    if (past >= now) {
+      alert('ERROR: La fecha que ingreso es MAYOR a la fecha actual');
+    return false;
     }
-
-    $('#departamentos').html(departmento);
-    $('select').formSelect();
-    $('#departamentos').on('change', function () {
-      var municipio = '<option value="" disabled selected>Municipio</option>';
-      for (let item of json[$(this).val()].ciudades) {
-        municipio += `<option value="${json[$(this).val()].ciudades.indexOf(item)}">${item}</option>`;
-      }
-      $('#municipios').html(municipio);
-      $('select').formSelect();
-    });
-  }
+    var nowYear = now.getFullYear();  //Get current year
+    var pastYear = past.getFullYear();//Get Date of Birth year
+    var age = nowYear - pastYear;  //calculate the difference
+$('#Age').val(age);
+if (age < 18){
+      alert('Debe ser Mayor de Edad para completar este formulario');
+    return false;
+    }
+  })
 })
 
+
+//Select Municipio segùn Departamento
+$.ajax({
+url: 'https://raw.githubusercontent.com/marcovega/colombia-json/master/colombia.min.json',
+type: 'GET',
+dataType: 'json',
+
+success: function (json) {
+  var departamento = `<option value="" disabled selected>Departamento</option>`;
+  for (let item of json) {
+    departamento += `<option value="${json.indexOf(item)}">${item.departamento}</option>`;
+  }
+
+  $('#departamentos').html(departamento);
+  $('select').formSelect();
+  $('#departamentos').on('change', function () {
+    var municipio = '<option value="" disabled selected>Municipio</option>';
+    for (let item of json[$(this).val()].ciudades) {
+      municipio += `<option value="${json[$(this).val()].ciudades.indexOf(item)}">${item}</option>`;
+    }
+    $('#municipios').html(municipio);
+    $('select').formSelect();
+  });
+}
+
+})
+
+
+
+
+
+function checkforblank() {
+    
+  var location = document.getElementById('Location');
+  var invalid = location.value == "Please Select";
+
+  if (invalid) {
+      alert('Please enter first name');
+      location.className = 'error';
+  }
+  else {
+      location.className = '';
+  }
+  
+  return !invalid;
+}
 
   function validarFormulario(evento) {
     evento.preventDefault(); 
 
-    var nombre, apellido, email, emailValidator, password, confirm_password, user, passwordValidator, phoneValidator, phone;
+    var nombre, apellido, email, emailValidator, password, confirm_password, user, passwordValidator, phoneValidator;
 
     user = document.getElementById('user').value;
-    phone = document.getElementById('phone').value;
+    location1 = document.getElementById('departamentos');
+    invalid1 = location1.value == "";
+    location2 = document.getElementById('municipios');
+    invalid2 = location2.value == "";
+    municipios = document.getElementById('municipios');
+    phone = document.forms["formulario"]["phone"].value;
     email = document.forms["formulario"]["mail"].value;
     nombre = document.getElementById('nombre').value;
     apellido = document.getElementById('apellido').value;
@@ -74,22 +105,21 @@ document.addEventListener("DOMContentLoaded", function() {
     else if(apellido.length == 0) {
       alert('Ingrese sus Apellidos completos');
       return;
-    }   
+    } 
     else if(email.length == 0) {
       alert('Ingrese un Correo Electrónico');
       return;
     }   
-    else if (!(email.match(emailValidator)))
-    {
+    else if (!(email.match(emailValidator))) {
       alert('CORREO ELECTRONICO INVALIDO. Recuerde que el formato para el correo es email@ejemplo.com - No debe contener caracteres extraños: (! # $ % & * + / = ? ^ { | } ~)');
       return;
     }
-    else if(phone.value.length == 0) {
-      alert('Ingrese un nombre de Usuario');
+    else if(phone.length == 0) {
+      alert('Ingrese un Número telefónico');
       return;
-    }
-    else if (!(phone.match(phoneValidator))) {
-      alert('TELÉFONO INVALIDA. Recuerde digitar su número de la siguiente manera: (Indicativo) + 7 dígitos de su residencia');
+    }   
+    else if (!(phone.match(phoneValidator))){
+      alert('TELÉFONO INVALIDO. Recuerde digitar su número de la siguiente manera: (Indicativo) + 7 dígitos de su residencia');
       return;
     }
     else if(user.length == 0) {
@@ -116,13 +146,19 @@ document.addEventListener("DOMContentLoaded", function() {
       alert('La Contraseña que esta Validando no coincide con la previamente escrita');
       return;
     }
-    
+    else if (invalid1) {
+      alert('Seleccione el Departamento en el que reside');
+      return
+    }
+    else if (invalid2) {
+      alert('Seleccione el Municipio en el que reside');
+      return;
+  }
 
  //El formulario se envia
  alert("Muchas gracias por completar el formulario");
- document.formulario.post();
-} 
-
-function updateTextInput(val) {
-  document.getElementById('textInput').value=val; 
-}
+ $(document.formulario).ready(function(){{
+    $("#pageloader").fadeIn();
+  };//submit
+//document ready
+} )}
